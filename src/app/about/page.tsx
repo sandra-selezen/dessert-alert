b79e9ea50a1cf6ce217db8298a4e25d909132c7a
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2024-11-28 19:40:23 +0200

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,12 @@
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <main>
+      <div className="containet max-w-max mx-auto text-center">
+        <p className="font-fredoka font-semibold text-black50">Home <span className="text-pink100">/ About</span></p>
+        <h1 className="h1">About</h1>
+      </div>
+    </main>
   )
 }
 
